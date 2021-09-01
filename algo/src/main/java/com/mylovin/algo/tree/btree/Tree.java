532fe819134b8c5e33f9d9f9215504ebb1494b49
861008761 <1992yang>
2021-09-01 19:47:28 +0800

    二叉平衡树

--- algo/src/main/java/com/mylovin/algo/tree/btree/Tree.java
+++ algo/src/main/java/com/mylovin/algo/tree/btree/Tree.java
@@ -5,6 +5,7 @@ public class Tree {
 
     public void insert(int data) {
         root = insert0(root, data);
+        root = rotate(root);
     }
 
     private Node insert0(Node node, int data) {
@@ -16,11 +17,13 @@ public class Tree {
         } else if (data > node.getData()) {
             node.setRight(insert0(node.getRight(), data));
         }
+        node = rotate(node);
         return node;
     }
 
     public void delete(int data) {
         root = delete0(root, data); // 赋值是为了保证删除的节点是根节点不出现问题
+        root = rotate(root);
     }
 
     private Node delete0(Node node, int data) {
@@ -48,9 +51,11 @@ public class Tree {
         }
         if (data < node.getData()) { // 待删除数据比当前小
             node.setLeft(delete0(node.getLeft(), data));
+            node = rotate(node);
         }
         if (data > node.getData()) { // 待删除数据比当前大
             node.setRight(delete0(node.getRight(), data));
+            node = rotate(node);
         }
         return node;
     }
@@ -61,6 +66,55 @@ public class Tree {
         }
         node.setHeight(Math.max(getHeight(node.getLeft()), getHeight(node.getRight())) + 1);
         return node.getHeight();
+    }
+
+    private Node LLRotate(Node node) {
+        Node leftChild = node.getLeft();
+        Node leftChild_right = leftChild.getRight();
+        leftChild.setRight(node);
+        node.setLeft(leftChild_right);
+        return leftChild;
+    }
+
+    private Node RRRotate(Node node) {
+        Node rightChild = node.getRight();
+        Node rightChild_left = rightChild.getLeft();
+        rightChild.setLeft(node);
+        node.setRight(rightChild_left);
+        return rightChild;
+    }
+
+    private Node LRRotate(Node node) {
+        node.setLeft(RRRotate(node.getLeft()));
+        return RRRotate(node);
+    }
+
+    private Node RLRotate(Node node) {
+        node.setRight(LLRotate(node.getRight()));
+        return LLRotate(node);
+    }
+
+    /**
+     * 平衡二叉树
+     *
+     * @param node
+     */
+    private Node rotate(Node node) {
+        if (getHeight(node.getLeft()) - getHeight(node.getRight()) >= 2) { // 左子树高于右子树
+            if (getHeight(node.getLeft().getLeft()) > getHeight(node.getLeft().getRight())) { // LL型，进行一次LL旋转
+                node = LLRotate(node);
+            } else {
+                node = LRRotate(node);
+            }
+        }
+        if (getHeight(node.getRight()) - getHeight(node.getLeft()) >= 2) {
+            if (getHeight(node.getRight().getRight()) > getHeight(node.getRight().getLeft())) { // RR型，进行一次RR旋转
+                node = RRRotate(node);
+            } else {
+                node = RLRotate(node);
+            }
+        }
+        return node;
     }
 
     public void printPretty() {
@@ -74,6 +128,7 @@ public class Tree {
         array[rootx][rooty] = root.getData();
         printPretty(root, height, array);
         printNow(height, width, array);
+        System.out.println();
     }
 
     private void printPretty(Node node, int height, int[][] array) {
@@ -109,21 +164,42 @@ public class Tree {
     public static void main(String[] args) {
         Tree tree = new Tree();
         tree.insert(50);
+        tree.printPretty();
         tree.insert(30);
+        tree.printPretty();
         tree.insert(20);
+        tree.printPretty();
         tree.insert(40);
+        tree.printPretty();
         tree.insert(35);
+        tree.printPretty();
         tree.insert(25);
+        tree.printPretty();
         tree.insert(10);
+        tree.printPretty();
         tree.insert(45);
+        tree.printPretty();
         tree.insert(80);
+        tree.printPretty();
         tree.insert(60);
+        tree.printPretty();
         tree.insert(55);
+        tree.printPretty();
         tree.insert(75);
+        tree.printPretty();
         tree.insert(90);
+        tree.printPretty();
         tree.insert(85);
+        tree.printPretty();
         tree.insert(95);
-        //tree.delete(50);
+        tree.printPretty();
+        tree.insert(1);
+        tree.printPretty();
+        tree.insert(39);
+        tree.printPretty();
+        tree.insert(67);
+        tree.printPretty();
+        tree.delete(60);
         tree.printPretty();
     }
 }
